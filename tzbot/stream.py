99a33eccae271cfe2468053c02feabc1bb8b0a88
Yasyf Mohamedali <yasyfm@gmail.com>
2017-02-13 02:20:42 -0500

    Make reply actual reply.

--- tzbot/stream.py
+++ tzbot/stream.py
@@ -6,7 +6,8 @@ USERNAME = os.getenv('TWITTER_USERNAME')
 class StreamListener(tweepy.StreamListener):
   def on_status(self, status):
     if status.user.screen_name != USERNAME and not status.retweeted:
-      self.api.update_status('@{} {} to you too!'.format(status.user.screen_name, status.text))
+      message = '@{} {} to you too!'.format(status.user.screen_name, status.text)
+      self.api.update_status(message, status.id)
 
 class Stream(object):
   def __init__(self):
